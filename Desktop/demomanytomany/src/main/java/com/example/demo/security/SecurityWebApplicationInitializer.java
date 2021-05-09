4d586866405346f3dd64519e80bfe3e5ed84b054
tarunya882 <80250798+tarunya882@users.noreply.github.com>
2021-05-09 23:10:03 +0530

    Delete Desktop directory

--- Desktop/demomanytomany/src/main/java/com/example/demo/security/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
